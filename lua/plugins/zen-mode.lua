4a94a9d70615da55231d91f6892aee7781d598dc
Adam Fowler <cockytrumpet@icloud.com>
2024-02-25 12:52:09 -0500

    add zen-mode defaults

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -1,6 +1,19 @@
 local M = {
   'folke/zen-mode.nvim',
   event = 'VeryLazy',
+  opts = {
+    window = {
+      options = {
+        signcolumn = 'yes', -- disable signcolumn
+        number = true, -- disable number column
+        relativenumber = true, -- disable relative numbers
+        cursorline = false, -- disable cursorline
+        cursorcolumn = false, -- disable cursor column
+        foldcolumn = 'auto', -- disable fold column
+        list = false, -- disable whitespace characters
+      },
+    },
+  },
 }
 
 return M
