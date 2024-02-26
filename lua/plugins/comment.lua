77b23177e7c17bc4d6d63253bcbb1ddc06005f73
Adam Fowler <cockytrumpet@icloud.com>
2024-02-26 11:05:27 -0500

    comment spec

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,13 +1,26 @@
 local M = {
   'numToStr/Comment.nvim',
   event = 'BufRead',
-  opts = {},
-  config = function()
+  opts = {
+    {
+      padding = true,
+      sticky = true,
+      ignore = nil,
+      toggler = { line = 'gcc', block = 'gbc' },
+      opleader = { line = 'gc', block = 'gb' },
+      extra = { above = 'gcO', below = 'gco', eol = 'gcA' },
+      mappings = { basic = true, extra = true },
+      pre_hook = nil,
+      post_hook = nil,
+    },
+  },
+  config = function(opts)
     vim.keymap.set('n', '<leader>/', function()
       require('Comment.api').toggle.linewise.current()
     end, { desc = 'Toggle comment' })
-
     vim.keymap.set('v', '<leader>/', "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", { desc = 'Toggle comment' })
+
+    require('Comment').setup(opts)
   end,
 }
 
