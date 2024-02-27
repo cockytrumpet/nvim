b3d8ecf30d75fb0a4afc8b7fddea3b3c1919ce13
Adam Fowler <cockytrumpet@icloud.com>
2024-02-27 18:15:34 -0500

    lualine: add lsp info

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,18 +9,33 @@ local function diff_source()
   end
 end
 
---    lualine_b = { {'diff', source = diff_source}, },
+-- TODO: replace deprecated vim functions
+local function lsp()
+  local msg = ''
+  local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+  local clients = vim.lsp.get_active_clients()
+  if next(clients) == nil then
+    return msg
+  end
+  for _, client in ipairs(clients) do
+    local filetypes = client.config.filetypes
+    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+      return client.name
+    end
+  end
+  return msg
+end
+
 local M = {
   'nvim-lualine/lualine.nvim',
   lazy = false,
   priority = 999,
   dependencies = { 'nvim-tree/nvim-web-devicons', 'AndreM222/copilot-lualine' },
   config = function()
+    ---@diagnostic disable-next-line: undefined-field
     require('lualine').setup {
       options = {
         theme = 'catppuccin',
-        section_separators = { '', '' },
-        component_separators = { '', '' },
       },
       sections = {
         lualine_a = { 'mode' },
@@ -54,6 +69,7 @@ local M = {
             show_loading = true,
           },
           'filetype',
+          { lsp },
         },
         lualine_y = { 'progress' },
         lualine_z = { 'location' },
