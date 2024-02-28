e85cda841439fe91fae6caa7e0b044a59803f065
Adam Fowler <cockytrumpet@icloud.com>
2024-02-27 23:24:17 -0500

    add treesitter-textobjects and change keybind for treesj

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -2,7 +2,7 @@ local M = {
   'Wansmer/treesj',
   event = 'BufWinEnter',
   config = function()
-    vim.keymap.set('n', '<leader>s', '<CMD>TSJToggle<CR>', { desc = 'split/join' })
+    vim.keymap.set('n', '<leader>j', '<CMD>TSJToggle<CR>', { desc = 'split/join' })
     require('treesj').setup {
       use_default_keymaps = false,
     }
