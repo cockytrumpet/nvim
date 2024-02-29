9052ef04d3064ebfb4b0e70c2e71fdb7ae721a2b
Adam Fowler <cockytrumpet@icloud.com>
2024-02-29 14:42:04 -0500

    keymap

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -2,6 +2,7 @@
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  desc = 'Highlight when yanking (copying) text',
   callback = function()
     vim.highlight.on_yank()
   end,
--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -1,6 +1,11 @@
 local M = {
   'nvim-treesitter/nvim-treesitter-context',
   event = 'BufWinEnter',
+  init = function()
+    vim.keymap.set('n', '[u', function()
+      require('treesitter-context').go_to_context(vim.v.count1)
+    end, { silent = true })
+  end,
   opts = {
     throttle = true,
     patterns = {
