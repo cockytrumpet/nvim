77b7cf541958fad461d3a752031aaee628e6a85e
Adam Fowler <cockytrumpet@icloud.com>
2024-03-02 11:34:37 -0500

    lsp_timeout: disabling, can't restart copilot

--- lua/plugins/lsp-timeout.lua
+++ lua/plugins/lsp-timeout.lua
@@ -1,8 +1,21 @@
 -- kill lsps that haven't been used in a while
 local M = {
   'hinell/lsp-timeout.nvim',
+  enabled = false,
   event = 'VeryLazy',
   dependencies = { 'neovim/nvim-lspconfig' },
+  setup = function()
+    vim.g.lspTimeoutConfig = {
+      stopTimeout = 1000 * 60 * 5, -- ms, timeout before stopping all LSPs
+      startTimeout = 1000 * 10, -- ms, timeout before restart
+      silent = false, -- true to suppress notifications
+      filetypes = {
+        ignore = { -- filetypes to ignore; empty by default
+          'help',
+        }, -- for these filetypes
+      },
+    }
+  end,
 }
 
 return M
