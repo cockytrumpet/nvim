c122d24a6fba2ad9214705a723e3c963c91136a8
Adam Fowler <cockytrumpet@icloud.com>
2024-03-09 21:32:00 -0500

    which-key: make some items prettier

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -8,13 +8,15 @@ local M = { -- Useful plugin to show you pending keybinds.
     require('which-key').register {
       ['<leader>b'] = { name = '[b]uffer', _ = 'which_key_ignore' },
       ['<leader>c'] = { name = '[c]ode', _ = 'which_key_ignore' },
-      ['<leader>cc'] = { name = '[cc]opilot chat', _ = 'which_key_ignore' },
+      ['<leader>cc'] = { name = '[c]opilot chat', _ = 'which_key_ignore' },
       ['<leader>d'] = { name = '[d]iagnostic/debug', _ = 'which_key_ignore' },
       ['<leader>D'] = { name = '[D]ad-bod', _ = 'which_key_ignore' },
       ['<leader>f'] = { name = '[f]ind', _ = 'which_key_ignore' },
       ['<leader>g'] = { name = '[g]it', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[r]un', _ = 'which_key_ignore' },
       ['<leader>t'] = { name = '[t]ab/term/todo', _ = 'which_key_ignore' },
+      ['<leader>tr'] = { name = '[r]un', _ = 'which_key_ignore' },
+      ['<leader>w'] = { name = '[w]indow', _ = 'which_key_ignore' },
       ['<leader>z'] = { name = '[z]en mode', _ = 'which_key_ignore' },
     }
   end,
