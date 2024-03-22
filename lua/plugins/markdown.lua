54d375e1bc14bb68a4b8e58cf71b240dc93e2a73
Adam Fowler <cockytrumpet@icloud.com>
2024-03-22 12:12:37 -0400

    add markdown.nvim

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,10 @@
+local M = {
+  'MeanderingProgrammer/markdown.nvim',
+  -- name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  config = function()
+    require('render-markdown').setup {}
+  end,
+}
+
+return M
