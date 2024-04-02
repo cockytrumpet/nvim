f04ea3d0fbe9c988c21f8ed3a16ea5017bbfdad9
Adam Fowler <cockytrumpet@icloud.com>
2024-04-02 16:22:31 -0400

    YtAudio: moved keymaps to lazy spec

--- lua/plugins/YtAudio.lua
+++ lua/plugins/YtAudio.lua
@@ -9,6 +9,12 @@ local M = {
 
 local M = {
   'cockytrumpet/YtAudio',
+  -- branch = 'now_playing',
+  init = function()
+    vim.api.nvim_set_keymap('n', '<leader>yp', ':YAPlay<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>ys', ':YAStop<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>y1', ':YAFav https://www.youtube.com/watch?v=abUT5IEkwrg<CR>', { noremap = true, silent = true })
+  end,
   event = 'VeryLazy',
   opts = true,
 }
