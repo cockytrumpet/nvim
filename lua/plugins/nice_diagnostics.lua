304e08a4ef7dcccf74409db8e1b85c44ef797605
Adam Fowler <cockytrumpet@icloud.com>
2024-07-03 16:42:40 -0400

    trying to get the formatting of tiny-inline-diagnostics without hiding

---
+++ lua/plugins/nice_diagnostics.lua
@@ -0,0 +1,67 @@
+-- toggle dev/live mode
+local mode = 'dev'
+
+local dev = {
+  dir = '~/dev/lua/plugins/nice-diagnostics',
+  name = 'nice-diagnostics',
+}
+
+local live = {
+  dir = '~/dev/lua/plugins/nice-diagnostics',
+  name = 'nice-diagnostics',
+}
+
+local get_opts = function()
+  if mode == 'dev' then
+    return dev
+  else
+    return live
+  end
+end
+
+local M = {
+  event = 'VeryLazy',
+  enabled = false,
+  config = function()
+    local mocha = require('catppuccin.palettes').get_palette 'mocha'
+    require('nice-diagnostics').setup {
+      signs = {
+        left = '',
+        right = '',
+        diag = '●',
+        arrow = '    ',
+        up_arrow = '    ',
+        vertical = ' │',
+        vertical_end = ' └',
+      },
+      hi = {
+        error = 'DiagnosticError',
+        warn = 'DiagnosticWarn',
+        info = 'DiagnosticInfo',
+        hint = 'DiagnosticHint',
+        arrow = 'NonText',
+        background = mocha.background, -- Can be a highlight or a hexadecimal color (#RRGGBB)
+        mixing_color = 'None', -- Can be None or a hexadecimal color (#RRGGBB). Used to blend the background color with the diagnostic background color with another color.
+      },
+      blend = {
+        factor = 0.27,
+      },
+      options = {
+        -- The minimum length of the message, otherwise it will be on a new line.
+        softwrap = 15,
+
+        --- When overflow="wrap", when the message is too long, it is then displayed on multiple lines.
+        overflow = 'wrap',
+
+        --- Enable it if you want to always have message with `after` characters length.
+        break_line = {
+          enabled = false,
+          after = 30,
+        },
+      },
+    }
+  end,
+}
+
+M = vim.tbl_extend('keep', get_opts(), M)
+return M
